b1ececbe63812c3fe6a576942cbf45504eccf7c7
Vlad-0213 <61988430+Vlad-0213@users.noreply.github.com>
2020-11-30 12:36:46 -0700

    fgf

---
+++ Assigment191/Assigment191/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment191
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "Vlad";
+            string quote = "The player hit a hummer!, \\a 420ft hummer!\", Longest of the season. \n Number 30hr of the year. \n \t he's on pace to 50 plus this year.";
+            string fileName = @"c:\user\2020stats";
+
+            name = name.ToUpper();
+
+            Console.WriteLine(name);
+            Console.ReadLine();
+                
+        }
+    }
+}
