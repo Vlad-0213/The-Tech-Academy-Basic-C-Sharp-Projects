e6d251550ef4cb70784faf3c37eaf015384e1248
Vlad-0213 <61988430+Vlad-0213@users.noreply.github.com>
2020-12-07 13:17:04 -0700

    198

---
+++ Assigment 194/Assigment 198/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Program
+    {
+    static void Main()
+
+    {
+
+        List<int> intList = new List<int>();
+        intList.Add(4);
+        intList.Add(10);
+        intList.Remove(10);
+
+        Console.WriteLine(intList[0]);
+        Console.ReadLine();
+
+        List<string> players = new List<string>();
+        players.Add("Peter");
+        players.Add("Lucas");
+        players.Remove("Mark");
+
+        Console.WriteLine(players[0]);
+        Console.ReadLine();
+
+
+
+        int[] numArray1 = {6, 9, 10, 100, 4000, 8000};
+        Console.WriteLine(numArray1[0]);
+        Console.ReadLine();
+
+         }
+}
